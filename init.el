6780abd95cae305dff5d0ab0ee33056bfba4e507
Motoki Shakagori <shakagorimotoki@realglobe.jp>
2018-12-14 19:30:55 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,42 @@
+(require 'cask)
+(cask-initialize)
+
+; intero
+(add-hook 'haskell-mode-hook 'intero-mode)
+
+; make enable clipboard copy
+(cond (window-system
+  (setq x-select-enable-clipboard t)
+  ))
+
+; do not create backup file
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(global-flycheck-mode)
+
+;; C-n, C-pで補完候補を次/前の候補を選択
+(require 'company)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'dracula t)
+
+; magit
+(require 'magit)
+(define-key global-map (kbd "M-g") 'magit-status)
